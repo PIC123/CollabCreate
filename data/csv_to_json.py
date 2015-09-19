4fc82727e33b03a287c293d842a2aa317023180a
Andrew Titus <atitus@mit.edu>
2015-09-19 17:15:00 -0400

    Fixed skills JSON conversion and imported to Parse

--- data/csv_to_json.py
+++ data/csv_to_json.py
@@ -5,5 +5,5 @@ with open("skills.csv", "r") as csv_file:
     with open("skills.json", "w") as json_file:
         fieldnames = ("id", "name")
         reader = csv.DictReader(csv_file, fieldnames)
-        json_file.write(json.dumps([row["name"] for row in reader]))
+        json_file.write(json.dumps({"results": [{"id": skill["id"], "name": skill["name"]} for skill in reader]}))
         print "Done"
