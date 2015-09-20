6ee49c3160814bd92e496e5141099729be2cbd10
Stewart Adam <s.adam@diffingo.com>
2015-09-19 21:47:29 -0400

    Setup pageReady() for navigational event handling

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,35 @@
-CollabCreate = {}
+CollabCreate = {};
+CollabCreate.pageReady = {};
+CollabCreate.pageReady.document = function () {
+  // Render basic page layout
+  var compiledTemplate = Handlebars.getTemplate('navbar');
+  var navbarHtml = compiledTemplate({ profileUrl : 'images/profile.png' });
+  jQuery('#render-navbar').html(navbarHtml)
+
+  var url = document.location.hash ? document.location.hash.substring(1) : 'home';
+  CollabCreate.navigate(url);
+
+  // Trigger page-specific reactions
+  jQuery("#render-content").on("navigated", function(e, oldUrl, newUrl) {
+    if (CollabCreate.pageReady.hasOwnProperty(newUrl)) {
+      CollabCreate.pageReady[newUrl]();
+    }
+  });
+
+  // Single-page app navigation
+  jQuery('body').on("click", 'a', function(e) {
+    var link = jQuery(this);
+    if (link.attr('rel') == 'external') {
+      window.open(jQuery(this).attr('href'));
+    }
+    else {
+      var url = link.attr('href');
+      CollabCreate.navigate(url);
+    }
+    e.preventDefault();
+  });
+};
+
 CollabCreate.navigate = function(url) {
   if (url == "") {
     url = "home";
@@ -16,28 +47,10 @@ CollabCreate.navigate = function(url) {
     var contentHtml = compiledTemplate({});
     jQuery('#render-content').html(contentHtml);
   }
+
+  var oldUrl = document.location.hash.substring(1);
   document.location.hash = url;
+  jQuery('#render-content').trigger("navigated", [oldUrl, url]);
 }
 
-jQuery(document).ready(function () {
-  // Render basic page layout
-  var compiledTemplate = Handlebars.getTemplate('navbar');
-  var navbarHtml = compiledTemplate({ profileUrl : 'images/profile.png' });
-  jQuery('#render-navbar').html(navbarHtml)
-
-  var url = document.location.hash ? document.location.hash.substring(1) : 'home';
-  CollabCreate.navigate(url);
-
-  // Single-page app navigation
-  jQuery('body').on("click", 'a', function(e) {
-    var link = jQuery(this);
-    if (link.attr('rel') == 'external') {
-      window.open(jQuery(this).attr('href'));
-    }
-    else {
-      var url = link.attr('href');
-      CollabCreate.navigate(url);
-    }
-    e.preventDefault();
-  });
-});
+jQuery(document).ready(CollabCreate.pageReady.document);
